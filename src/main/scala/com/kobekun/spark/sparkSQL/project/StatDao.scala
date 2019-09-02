6d1fa3011acd0a8eae2ee9bd2de34acaa5d2c63a
kobekun <kobekun881105@126.com>
2019-09-02 23:56:18 +0800

    按天、地市、流量统计视频访问的dao层

--- src/main/scala/com/kobekun/spark/sparkSQL/project/StatDao.scala
+++ src/main/scala/com/kobekun/spark/sparkSQL/project/StatDao.scala
@@ -43,5 +43,82 @@ object StatDao {
   }
 
 
+  /**
+    * 批量保存DayCityVideoAccessTopNStat到数据库
+    */
+
+    def insertDayCityVideoAccessTopNStat(list: ListBuffer[DayCityVideoAccessStat]): Unit ={
+
+      var conn: Connection = null
+      var pstmt: PreparedStatement = null
+
+      try{
+
+        conn = MysqlUtils.getConnection()
+
+        //自动提交改成手工提交
+        conn.setAutoCommit(false)
+
+        val sql = "insert into  day_city_video_access_topn_stat(day,city,cms_id,times,times_rank) values(?,?,?,?,?)"
+        pstmt = conn.prepareStatement(sql)
+
+        for(ele <- list){
+          pstmt.setString(1,ele.day)
+          pstmt.setString(2,ele.city)
+          pstmt.setLong(3,ele.cmsId)
+          pstmt.setLong(4,ele.times)
+          pstmt.setInt(5,ele.timesRank)
+
+          pstmt.addBatch()
+        }
+
+        pstmt.executeBatch() // 执行批量处理
+        conn.commit() // 手动提交
+
+      }catch {
+        case e: SQLException => e.printStackTrace()
+      }finally {
+        MysqlUtils.releaseResource(conn,pstmt)
+      }
+    }
+
+
+
+  /**
+    * 批量保存DayTrafficVideoAccessTopNStat到数据库
+    */
+
+  def insertDayTrafficVideoAccessTopNStat(list: ListBuffer[DayTrafficVideoAccessStat]): Unit ={
+
+    var conn: Connection = null
+    var pstmt: PreparedStatement = null
+
+    try{
+
+      conn = MysqlUtils.getConnection()
+
+      //自动提交改成手工提交
+      conn.setAutoCommit(false)
+
+      val sql = "insert into  day_traffic_video_access_topn_stat(day,cms_id,traffics) values(?,?,?)"
+      pstmt = conn.prepareStatement(sql)
+
+      for(ele <- list){
+        pstmt.setString(1,ele.day)
+        pstmt.setLong(2,ele.cmsId)
+        pstmt.setLong(3,ele.traffics)
+
+        pstmt.addBatch()
+      }
+
+      pstmt.executeBatch() // 执行批量处理
+      conn.commit() // 手动提交
+
+    }catch {
+      case e: SQLException => e.printStackTrace()
+    }finally {
+      MysqlUtils.releaseResource(conn,pstmt)
+    }
+  }
 
 }
