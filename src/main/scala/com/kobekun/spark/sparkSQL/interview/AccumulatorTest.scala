abf4723d9fa91ed484a1f6fb68f59c89705e5fa7
kobekun <kobekun881105@126.com>
2019-09-26 22:27:13 +0800

    累加器

---
+++ src/main/scala/com/kobekun/spark/sparkSQL/interview/AccumulatorTest.scala
@@ -0,0 +1,23 @@
+package com.kobekun.spark.sparkSQL.interview
+
+import org.apache.spark.sql.SparkSession
+
+object AccumulatorTest {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder().master("local[2]")
+      .appName("AccumulatorTest").getOrCreate()
+    val sc = spark.sparkContext
+
+    val accum = sc.longAccumulator("longAccum") //统计奇数的个数
+
+    val sum = sc.parallelize(Array(1,2,3,4,5,6,7,8,9),2).filter(n=>{
+      if(n%2!=0) accum.add(1L)
+      n%2==0
+    }).reduce(_+_)
+
+    println("sum: "+sum)
+    println("accum: "+accum.value)
+  }
+}
