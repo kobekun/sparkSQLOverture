0ab62d834afdefabad78af136d2eda84b4c08507
kobekun <kobekun881105@126.com>
2019-11-12 23:13:27 +0800

    kafka问题

---
+++ src/main/scala/com/kobekun/spark/sparkSQL/interview/kafka/OffsetOne.scala
@@ -0,0 +1,36 @@
+package com.kobekun.spark.sparkSQL.interview.kafka
+
+import org.apache.spark.SparkConf
+
+object OffsetOne {
+
+
+  def main(args: Array[String]): Unit = {
+
+    val sparkConf = new SparkConf().setAppName("OffsetOne").setMaster("local[2]")
+
+    val ssc = new StreamingContext(sparkConf, Seconds(10))
+
+    val kafkaParams = Map[String, String](
+      "metadata.broker.list" -> "hadoop000:9092",
+      //本应用程序如果中断，下次启动时偏移量从头开始消费
+      "auto.offset.reset" -> "smallest"
+    )
+
+    val topics = "imooc_pk_offset".split(",").toSet
+
+    val msg = KafkaUtils.createDirectStream[String,String,StringDecoder,StringDecoder](ssc,
+      kafkaParams,topics)
+
+    msg.foreachRDD(rdd => {
+
+      if(!rdd.isEmpty()){
+
+        println("kobekun: " + rdd.count())
+
+      }
+    })
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
